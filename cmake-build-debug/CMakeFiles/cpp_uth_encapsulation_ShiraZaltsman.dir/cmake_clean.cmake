e8598210dd0bfd97246613a6c287fa61a67fbebb
ShiraZaltsman <shira.zaltsman@gmail.com>
2019-09-25 18:27:54 +0300

    test

---
+++ cmake-build-debug/CMakeFiles/cpp_uth_encapsulation_ShiraZaltsman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_uth_encapsulation_ShiraZaltsman.dir/main.c.o"
+  "cpp_uth_encapsulation_ShiraZaltsman.pdb"
+  "cpp_uth_encapsulation_ShiraZaltsman"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cpp_uth_encapsulation_ShiraZaltsman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cpp_uth_encapsulation_ShiraZaltsman.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpp_uth_encapsulation_ShiraZaltsman.
+# This may be replaced when dependencies are built.
